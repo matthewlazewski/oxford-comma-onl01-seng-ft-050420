72a6158dbf1b8880eaf2e048dab5b78dbea20766
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-05 21:00:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2 
+    array.join(" and ")
+  else  
+    array.insert(array.length-1, "and").join(", ")
+  end
 end
